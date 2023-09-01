e288414211c95082ae44cae73adfd5f8c919e17c
diogenmishel@gmail.com <diogenmishel@gmail.com>
2023-09-01 13:44:42 +0300

    Чистка пустых аккаунтов

--- DBChanger/Program.cs
+++ DBChanger/Program.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Mono.Data.Sqlite;
 using System.Data;
 using System.IO;
 using System.Data.Common;
+using System.ComponentModel.Design.Serialization;
 
 namespace DBChanger
 {
@@ -18,8 +20,237 @@ namespace DBChanger
         static void Main(string[] args)
         {
             //MainAsync().ConfigureAwait(false).GetAwaiter().GetResult();
-            RemoveTwinks().ConfigureAwait(false).GetAwaiter().GetResult();
+            ClearEmptyAccounts().ConfigureAwait(false).GetAwaiter().GetResult();
         }
+
+        #region Чистка дефектных аккаунтов
+        static async Task ClearEmptyAccounts()
+        {
+            InfoMessage("[DBChanger] Подключение к tshock.sqlite . . .");
+            tShockDb = await GetConnection(Path.Combine("tshock", "tshock.sqlite"));
+            if (tShockDb == null)
+            {
+                InfoMessage("[DBChanger] Полезный процесс завершен.");
+                await Task.Delay(-1);
+            }
+
+            InfoMessage("[DBChanger] Подключение к Minigames.sqlite . . .");
+            minigamesDb = await GetConnection(Path.Combine("tshock", "Minigames", "Minigames.sqlite"));
+            if (minigamesDb == null)
+            {
+                InfoMessage("[DBChanger] Полезный процесс завершен.");
+                await Task.Delay(-1);
+            }
+
+            LoadBadwords();
+            InfoMessage("[DBChanger] Получение списка пустых игроков . . .");
+            var list = await GetEmptiesList();
+            if (list == null || list.Count == 0)
+            {
+                InfoMessage("[DBChanger] Полезный процесс завершен.");
+                await Task.Delay(-1);
+            }
+
+            int count = 0;
+            InfoMessage("[DBChanger] Удаление отобранных игроков . . .");
+            foreach (var x in list)
+            {
+                if ((DateTime.Now - LastTitleUpdate).TotalMilliseconds > 100)
+                {
+                    Console.Title = $"Обработано результатов: {count}/{list.Count}";
+                    LastTitleUpdate = DateTime.Now;
+                }
+                if (await Delete(tShockDb, "Users", x.name))
+                    x.deleted++;
+                if (await Delete(minigamesDb, "Players", x.name))
+                    x.deleted++;
+                count++;
+            }
+
+            string results = 
+                " --- --- --- РЕЗУЛЬТАТЫ ЧИСТКИ ПУСТЫХ АККАУНТОВ --- --- ---" + '\n' +
+                '\n' +
+                '\n' +
+                '\n' +
+                " --- Условия удаления ---" + '\n' +
+                "Группа: default" + '\n' +
+                "Никнейм: некорректный" + '\n' +
+                "Активность: менее 10 дней с момента регистрации" + '\n' +
+                "Последний логин: больше 90 дней назад или никогда" + '\n' +
+                '\n' +
+                '\n' +
+                '\n' +
+                " --- Общие сведения ---" + '\n' +
+                $"Удалено: {list.Count(x => x.deleted == 2)}" + '\n' +
+                $"Удалено не полностью (1 ошибка): {list.Count(x => x.deleted == 1)}" + '\n' +
+                $"Не удалено (2 ошибки): {list.Count(x => x.deleted == 0)}" + '\n' +
+                '\n' +
+                '\n' +
+                '\n' +
+                " --- Список отобранных аккаунтов ---" + '\n' +
+                "  *  Name - Last Login - Registered - Reson" + '\n' +
+                string.Join("\n", list.Select(x => $"  *  {x.name} - {x.lastLogin} - {x.registered} - {x.reason}")) + '\n' +
+                '\n' +
+                '\n' +
+                '\n' +
+                $"Дата: {DateTime.Now.ToString()}, Автор программы: Диагенов Михаил";
+
+            File.WriteAllText($"{Environment.CurrentDirectory}\\DBChanger results.txt", results);
+            SuccessMessage("[DBChanger] Готово!");
+            await Task.Delay(-1);
+        }
+
+        static async Task<List<EmptyAccount>> GetEmptiesList()
+        {
+            var list = new List<EmptyAccount>();
+            using (var cmd = tShockDb.CreateCommand())
+            {
+                cmd.CommandText = $"SELECT * FROM `Users` WHERE `Usergroup`='default';";
+                try
+                {
+                    using (var r = await cmd.ExecuteReaderAsync())
+                        while (await r.ReadAsync())
+                        {
+                            var username = r.GetString(1);
+                            var uuid = r.IsDBNull(3) ? "" : r.GetString(3);
+                            var ips = r.IsDBNull(7) ? "" : r.GetString(7);
+
+                            var lastLogin = r.IsDBNull(6) ? 
+                                new DateTime() : 
+                                DateTime.Parse(r.GetString(6)).ToLocalTime();
+
+                            var registered = r.IsDBNull(5) ?
+                                new DateTime() :
+                                DateTime.Parse(r.GetString(5)).ToLocalTime();
+
+                            if (string.IsNullOrWhiteSpace(uuid) || string.IsNullOrWhiteSpace(ips))
+                            {
+                                list.Add(new EmptyAccount
+                                {
+                                    name = username,
+                                    lastLogin = lastLogin,
+                                    registered = registered,
+                                    reason = "пустой аккаунт"
+                                });
+                            }
+                            else if ((lastLogin - registered).TotalDays < 10 && (DateTime.Now - lastLogin).TotalDays > 90)
+                            {
+                                list.Add(new EmptyAccount
+                                {
+                                    name = username,
+                                    lastLogin = lastLogin,
+                                    registered = registered,
+                                    reason = "аккаунт однодневка"
+                                });
+                            }
+                            else if (!CheckNickname(username, out string reason))
+                            {
+                                list.Add(new EmptyAccount
+                                {
+                                    name = username,
+                                    lastLogin = lastLogin,
+                                    registered = registered,
+                                    reason = reason
+                                });
+                            }
+                        }
+                }
+                catch (DbException ex)
+                {
+                    ErrorMessage($"[Database] [tshock] Error: {ex.Message}");
+                }
+                catch (ArgumentException ex)
+                {
+                    ErrorMessage($"[Database] [tshock] Error: {ex.Message}");
+                }
+                catch (InvalidCastException ex)
+                {
+                    ErrorMessage($"[Database] [tshock] Error: {ex.Message}");
+                }
+            }
+            return list;
+        }
+
+        static bool CheckNickname(string username, out string reason)
+        {
+            var chars = username.ToList().FindAll(x => !"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#$%^&*()_+\"№;%:?-=\\/|[]{}'<>.,".Contains(x));
+            if (chars.Count != 0)
+            {
+                reason = "неклавиатурные символы в нике";
+                return false;
+            }
+            if (username.Length > 20)
+            {
+                reason = "длинный ник";
+                return false;
+            }
+            if (Regex.IsMatch(username, @"\[[icnag].+", RegexOptions.IgnoreCase)) // @"\[[icnag].+\]"
+            {
+                reason = "неклавиатурные символы в нике";
+                return false;
+            }
+            if (HasBadword(username))
+            {
+                reason = "плохие слова в нике";
+                return false;
+            }
+            reason = "";
+            return true;
+        }
+
+        public static Dictionary<char, List<string>> Badwords = new Dictionary<char, List<string>>();
+        public static Dictionary<char, char> SimilarChars = new Dictionary<char, char>
+        {
+            { 'x', 'х' },
+            { 'y', 'у' },
+            { 'o', 'о' },
+            { 'e', 'е' },
+            { 'c', 'с' },
+            { 'k', 'к' },
+            { 'a', 'а' },
+            { 'u', 'и' },
+        };
+
+        public static void LoadBadwords()
+        {
+            var path = Path.Combine("badwords.txt");
+            if (!File.Exists(path))
+                return;
+
+            Badwords.Clear();
+            foreach (var word in File.ReadAllLines(path))
+            {
+                if (!Badwords.ContainsKey(word[0]))
+                    Badwords.Add(word[0], new List<string>());
+                Badwords[word[0]].Add(word);
+            }
+        }
+
+        public static bool HasBadword(string text)
+        {
+            var words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length < 3)
+                    continue;
+                var lowerInvariant = words[i].ToLowerInvariant();
+
+                var translit = ' ' + lowerInvariant;
+                foreach (var x in SimilarChars)
+                {
+                    if (translit.Contains(x.Key))
+                        translit = translit.Replace(x.Key, x.Value);
+                }
+                lowerInvariant += translit;
+
+                if (Badwords.TryGetValue(lowerInvariant[0], out List<string> list) && list.Any(x => lowerInvariant.StartsWith(x)))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        #endregion
 
         #region Твинки на удаление
         static async Task RemoveTwinks()
@@ -814,6 +1045,27 @@ namespace DBChanger
             public override string ToString()
             {
                 return $"{name.Replace("'", "''")}\n{group}\n{lastLogin.ToString("s")}";
+            }
+        }
+    
+        class EmptyAccount
+        {
+            public string name;
+            public DateTime lastLogin;
+            public DateTime registered;
+            public byte deleted;
+            public string reason;
+
+            public string status
+            {
+                get
+                {
+                    if (deleted == 2)
+                        return "Удален";
+                    if (deleted == 1)
+                        return "Не полностью удален";
+                    return "Не удален";
+                }
             }
         }
     }
